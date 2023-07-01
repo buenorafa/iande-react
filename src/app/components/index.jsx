7846592cfc9348a1f2a6225008ff13f85d701e09
buenorafa <rafaelbuenooo@outlook.com>
2023-07-01 11:44:58 -0300

    exporta todos os components

---
+++ src/app/components/index.jsx
@@ -0,0 +1,6 @@
+export { default as Card } from "./Card";
+export { default as Contact } from "./Contact";
+export { default as Footer } from "./Footer";
+export { default as Link } from "./Link";
+export { default as NavBar } from "./NavBar";
+export { default as TitleSection } from "./TitleSection";
