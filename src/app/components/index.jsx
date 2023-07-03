de3e6615c0e91de985bec2740f7cb08bb040b12f
buenorafa <rafaelbuenooo@outlook.com>
2023-07-02 21:01:14 -0300

    :heavy_plus_sign: Phone e Email Input

--- src/app/components/index.jsx
+++ src/app/components/index.jsx
@@ -4,3 +4,5 @@ export { default as Footer } from "./Footer";
 export { default as Link } from "./Link";
 export { default as NavBar } from "./NavBar";
 export { default as TitleSection } from "./TitleSection";
+export { default as PhoneInput } from "./PhoneInput";
+export { default as EmailInput } from "./EmailInput";
